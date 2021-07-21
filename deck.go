521668e58cc053b93c371664ce1698c66a219a34
Kartik <kartik@hyperverge.co>
2021-07-21 23:50:02 +0530

    Add newDeck method

--- deck.go
+++ deck.go
@@ -5,6 +5,17 @@ import "fmt"
 // Create a new type of 'deck' which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuites := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suite)
+		}
+	}
+	return cards
+}
 func (cards deck) print() {
 	for i, card := range cards {
 		fmt.Println(i, card)
